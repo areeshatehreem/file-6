2b1346feb6f70ac694bcf0e9ebae4a784e80fba7
areeshatehreem <50449369+areeshatehreem@users.noreply.github.com>
2019-07-16 12:48:59 +0500

    wip

---
+++ script.js
@@ -0,0 +1,1044 @@
+let profession = ["doctor", "engineer", "astronomers"]
+
+
+
+let doctor = {
+
+    profession: "dentist",
+    salary: 215521,
+    houseJob: "eightYear",
+    isAnyScope: true,
+    yearsToBe: 4,
+    famousUniversity: [
+        "dowInternationalDentalCollege",
+        "bahriaUniversityMedicalAndDentalCollege"
+    ]
+
+}
+
+let Physiotherapist = {
+
+    profession: "Physiotherapist",
+    houseJob: " oneYear",
+    yearsToBe: 4,
+    yearsInDptProgram: 3,
+    salaryInMounth: 500,
+    salaryInYear: 497412,
+    isAnyScope: true,
+    famousUniversity: [
+        " liaquatNationalMedicalCollege",
+        " baqaiUniversity"
+    ]
+}
+
+
+let Dermatology = {
+    work: "treatmentOfDiseasesOfTheSkin,HairAndNails.",
+    scope: "moreDemanded",
+    salary: 246112,
+    jobs: [
+        "clicnic",
+        "hospitals",
+    ],
+    University: [
+        "agaKhanUniversity",
+        "dowUniversityOfHealthSciences",
+    ]
+
+}
+
+
+let DiagnosticRadiology = {
+
+    profession: "DiagnosticRadiology",
+    houseJob: " severalYearOfTraining",
+    yearsToBe: 4,
+    salaryInYear: 456712,
+    isAnyScope: true,
+    famousUniversity: [
+        " agaKhanUniversity",
+        " dowUniversityOfHealthSciences"
+    ],
+    work: "treatingDiseaseAndInjury,UsingMedicalImagingTechniquesSuchAsMRI,CT",
+
+
+
+}
+
+
+
+let Anesthesiology = {
+
+    profession: "Anesthesiology",
+    houseJobInYears: 2,
+    yearsToBe: 4,
+    salaryInYear: 497412,
+    isAnyScope: true,
+    famousUniversity: [
+        " universityOfKarachi",
+        " universityOfKarachi",
+    ]
+}
+
+
+
+
+let chemicalEngineer = {
+
+    profession: "chemicalEngineer",
+    yearsToBe: 4,
+    salaryInYear: 73294,
+    work: "chemicalEngineersDevelopAndDesignChemicalManufacturingprocesses.",
+    isGoodCareer: true,
+    kindsOfJob: "paper,petroChemicals,FoodProcessing,SpecialtyChemicals,",
+    famousUniversity: [
+        " dawoodUniversityOfEngineering&Technology",
+        "nedUniversityOfEngineeringAndTechnology",
+    ]
+}
+
+
+
+
+
+let mechanicalEngineer = {
+
+    profession: "mechanicalEngineer",
+    yearsToBe: 4,
+    salaryInYear: 43789,
+    isGoodCareer: true,
+    famousUniversity: [
+        " dawoodUniversityOfEngineering&Technology",
+        " nedUniversityOfEngineeringAndTechnology",
+    ]
+}
+
+
+
+
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: 59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+let aeronauticalEngineer = {
+
+    profession: "aeronauticalEngineer",
+    yearsToBe: 4,
+    salaryInYear: 160290,
+    isGoodCareer: true,
+    isEnjoable: true,
+    work: "AeronauticalEngineersWorkWithAircraft.",
+    famousUniversity: [
+        "nationalUniversityOfSciences&Technology(NUST),Islamabad.",
+        "pakistanInstituteOfEngineeringAndAppliedSciences(PIEAS) ",
+
+    ]
+}
+
+
+
+let electricalEngineer = {
+
+    profession: "electricalEngineer",
+    yearsToBe: 4,
+    salaryInYear: 594952,
+    isGoodCareer: true,
+    famousUniversity: [
+        "nationalUniversityOfSciences&Technology(NUST),Islamabad.",
+        "pakistanInstituteOfEngineeringAndAppliedSciences(PIEAS) ",
+
+
+
+
+    ]
+}
+
+
+
+let accountant = {
+
+    profession: "accountant",
+    yearsToBe: 4.5,
+    salaryInYear: 60000,
+    isGoodCareer: true,
+    scope: "isOnlySocpeInPakistan",
+    IsTtDifficultToPassCA: true,
+    work: "takeInformationAboutFiniancialRecords",
+    famousUniversity: [
+        "charteredAccountant(CA)Colleges&universitiesInPakistan",
+
+    ]
+}
+
+
+
+let fisherMan = {
+
+    profession: "fisherMan",
+    salaryInYear: 3000,
+    isGoodCareer: false,
+    work: "catchingFishes",
+
+}
+
+
+let teacher = {
+
+    profession: "teacher",
+    salaryInYear: undefined,
+    isGoodCareer: true,
+    subjects: undefined,
+    famousUniversity: undefined,
+
+
+
+}
+
+
+
+let Architect = {
+
+    profession: "Architect",
+    yearsToBe: 4,
+    payScaleInYear: 1500000,
+    isGoodCareer: true,
+    work: "design,PlanAndDevelop.",
+    famousUniversity: [
+        "indusValleySchoolOfArtAndArtitecture",
+    ]
+}
+
+
+let astronomers = {
+
+    profession: "astronomers",
+    yearsToBe: 4,
+    salaryInYear: 101630,
+    isGoodCareer: true,
+    work: "researchAboutUniverse",
+    famousUniversity: [
+        "instituteOfSpaceAndPlanetarySciences(ISPA),UniversityOfKarachi",
+        "departmentOfSpaceScience,UniversityOfPunjab.",
+
+    ]
+}
+
+
+
+let softwareEngineer = {
+
+    profession: "softwareEngineer",
+    yearsToBe: 4,
+    salaryInYear: 59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "usmanInstituteOfTechnology",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+
+let pilot = {
+
+    profession: "pilot",
+    yearsToBe: 2,
+    salaryInYear: 100000,
+    isGoodCareer: true,
+    isItHardToBeAPilot: false,
+    famousUniversity: [
+        "karachiAeroClub",
+        "kkAviationLtd",
+
+    ]
+}
+
+
+
+let fighterPilot = {
+
+    profession: "fighterPilot",
+    yearsToBe: 18 - 34,
+    salaryInYear: 90000,
+    isGoodCareer: true,
+    isItsTraingHard: true,
+    famousUniversity: [
+        "sapsAviationCollege",
+        "pakistanAirForceAcademy",
+
+    ]
+}
+
+
+let banker = {
+
+    profession: "banker",
+    yearsToBe: 4,
+    salaryInYear: 50000,
+    isGoodCareer: true,
+    famousUniversity: [
+        "karachiUniversity",
+        "bahriaUniversity"
+
+    ]
+}
+
+
+
+let lawyer = {
+
+    profession: "lawyer",
+    yearsToBe: 4,
+    salaryInYear: 80000,
+    work: "solveTheCaseUsingKnowledgeOfLegalPrecedents.",
+    degree: " juridicalScience,masterOfLaw",
+    isGoodCareer: true,
+    famousUniversity: [
+        "shaheedZulfiqarAliBhuttoUniversityOfLaw",
+        "sindhMuslimGovernmentLawCollege",
+
+    ]
+}
+
+
+
+let cheif = {
+
+    profession: "cheif",
+    yearsToBe: 2,
+    work: "cookingNewDishes",
+    salaryInYear: 39000,
+    isGoodCareer: true,
+    famousUniversity: [
+        "indusValleySchoolOfArtAndArchitecture",
+        "CMA - CollegeOfMedia&Arts",
+
+    ]
+}
+
+
+
+
+let nurse = {
+
+    profession: "nurse",
+    years: 1,
+    payScaleInYear: 5500,
+    isGoodCareer: true,
+    isAnyScope: false,
+    work: "takeCareofPatients",
+    nurseBy: "course",
+}
+
+
+let driver = {
+
+    profession: "driver",
+    salaryInYear: undefined,
+    isHaveLisence: true,
+    work: "pickAndDrop",
+    scope: "none",
+    isGoodCareer: false,
+
+
+}
+
+
+
+let painter = {
+
+    profession: "painter",
+    career: non,
+    salary: undefined,
+    work: "paintOnAnyPlace",
+    isRespectedJob: none,
+
+
+
+}
+
+
+let labour = {
+
+    profession: "labour",
+    isGoodCareer: false,
+    work: "constructAnyBuilding",
+    isneedUniversity: "none",
+    scope: false,
+
+
+}
+
+
+
+let businessMan = {
+
+    profession: "busineesMAn",
+    yearsToBe: 4,
+    salaryInYear: 100000,
+    isGoodCareer: true,
+    scope: true,
+    famousUniversity: [
+        "karachiUniversity",
+        "nust",
+
+    ]
+}
+
+
+
+let salesMan = {
+
+    profession: "salesMan",
+    salaryInYear: 2500,
+    isGoodCareer: false,
+    work: "toSalesThingsOnShop",
+    isRespestableJob: false,
+
+
+}
+
+
+let scientist = {
+
+    profession: "scientist",
+    yearsToBeInCollege: 4,
+    yearForMaster: 3,
+    salaryInYear: 100000,
+    isGoodCareer: true,
+    isDangerousJob: true,
+    famousUniversity: [
+        "fedralUrduUniversity",
+        "muhammadAliJinnahUniversity",
+
+
+    ],
+    skills: [
+        "Patience",
+        "determination",
+    ]
+}
+
+
+
+let police = {
+
+    profession: "police",
+    payScaleInYear: 4.5,
+    isGoodCareer: true,
+    scope: false,
+    isRespestableJob: " none",
+}
+
+
+
+let magician = {
+
+    profession: "magician",
+    work: "toEntertainOthers",
+    isEnjoyableJob: "maybe",
+    scope: "inMYOpinionNo",
+
+}
+
+
+let carpenter = {
+
+    profession: "carpenter",
+    salaryInYear: undefined,
+    isGoodCareer: false,
+    work: "makeFurnitureAndFixture",
+
+
+
+}
+
+
+let swimmers = {
+
+    profession: "swimmers",
+    work: "swimAndTeachesOther",
+    isDangerous: true,
+
+
+
+}
+
+
+
+let gardner = {
+
+    profession: "gardner",
+    scope: false,
+    work: "toGrowPlants",
+
+}
+
+
+let securityGaurd = {
+
+    profession: "securityGaurd",
+    work: "secureToEachOther",
+    responsiblity: "humansSecurity",
+    payment: 2500,
+    isGoodCareer: false,
+    isHaveGun: true,
+    isHaveGunLicience: true,
+}
+
+
+
+let importer = {
+
+    profession: "importer",
+    work: "importThingsFromDifferentCountry",
+    salaryInYear: 150000,
+    isGoodCareer: true,
+    scope: true,
+    vechicle: "byShipsAndRoad",
+}
+
+
+let vascularSurgeons = {
+
+    profession: "vascularSurgeons",
+    yearsToBe: 4,
+    trainingYear: 7,
+    salaryInYear: 90000,
+    isGoodCareer: true,
+    isHaveDemand: true,
+    famousUniversity: [
+        "dowUniversityOfHealthScience",
+        "liaquatNationalHosiptal",
+
+    ]
+}
+
+
+
+let pharmacist = {
+
+    profession: "pharmacist",
+    yearsToBe: 4,
+    trainingYear: 8,
+    salaryInYear: 70000,
+    isGoodCareer: true,
+    responsibility: "toMakeMedicience",
+    work: "researchForCommingNewMedicience",
+    famousUniversity: [
+        "baqaiuniversity",
+        "dowUniversity",
+
+    ]
+}
+
+
+
+let transportationEngineer = {
+
+    profession: "transportationEngineer",
+    yearsToBe: 6,
+    salaryInYear: 65495,
+    isGoodCareer: true,
+    famousUniversity: [
+        "indusUniversity",
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+
+    ]
+}
+
+
+
+let agricultureEngineer = {
+
+    profession: "agricultureEngineer",
+    yearsToBe: 4,
+    salaryInYear: 45000,
+    isGoodCareer: true,
+    isdemand: true,
+    isSmallOccpation: true,
+    famousUniversity: [
+        ". University Of Agriculture",
+        " Sindh Agriculture University",
+
+    ]
+}
+
+
+
+let petroleumEngineer = {
+
+    profession: "petroleumEngineer",
+    yearsToBe: 4,
+    payOff: "high",
+    isDifficult: true,
+    isGoodCareer: true,
+    famousUniversity: [
+        "Khawaja Fareed University Of Engineering & Information Technology",
+        ". Ghazi University",
+
+    ]
+}
+
+
+let exporter = {
+
+    profession: "exporter",
+    work: "exportThingFromCountries",
+    salaryInYear: 67000,
+    isGoodCareer: true,
+    scope: "moreScope",
+}
+
+
+let ArtificialIntelligence = {
+
+    profession: "ArtificialIntelligence",
+    yearsToBe: 4,
+    salaryInYear: 1700000,
+    isGoodCareer: true,
+    famousUniversity: [
+        "behriaUniversity",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+
+let networking = {
+
+    profession: "networking",
+
+    salaryInYear: 56000,
+    isGoodCareer: true,
+    famousUniversity: [
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+let graphics = {
+
+    profession: "graphics",
+    yearsToBe: 4,
+    salaryInYear: 45000,
+    isGoodCareer: true,
+    famousUniversity: [
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+
+let skinSurgeon = {
+
+    profession: "skinSurgeon",
+    yearsToBe: 4,
+    salaryInYear: 870000,
+    isGoodCareer: true,
+    work: "toCheckPatintentsOfSkinDisease",
+    famousUniversity: [
+        "dowUniversityOfHealthScience",
+        "hamdardUniversity",
+        "liaquatNationalUniversity",
+
+    ]
+}
+
+
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+
+let brainSurgeon = {
+
+    profession: "brainSurgeon",
+    yearsToBe: 4,
+    salaryInYear: 80000,
+    isGoodCareer: true,
+    work: "operationOfBrain",
+    famousUniversity: [
+        "dowUniversityOfHealthScience",
+        "liaquatNationalUniversity",
+        "hamdardUniversity"
+
+    ]
+}
+
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+let civilEngineer = {
+
+    profession: "civilEngineer",
+    yearsToBe: 4,
+    salaryInYear: $59494,
+    isGoodCareer: true,
+    famousUniversity: [
+        "mehranUniversityOfEngineering&Technology,Jamshoro",
+        "nedUniversityOfEngineeringAndTechnology",
+
+    ]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(profession);
+console.log(doctor);
+console.log(Anesthesiology);
+console.log(DiagnosticRadiology);
+console.log(Physiotherapist);
+console.log(Dermatology);
+console.log(civilEngineer);
+console.log(electricalEngineer);
+console.log(mechanicalEngineer);
+console.log(aeronauticalEngineer);
+console.log(teacher);
+console.log(Architect);
+console.log(fisherMan);
+console.log(astronomers);
+console.log(softwareEngineer);
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
